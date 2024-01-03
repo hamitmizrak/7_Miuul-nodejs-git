ebff51b5ba841994563a641579cb4d4ed6b108fe
Hamit Mizrak <hamitmizrak@gmail.com>
2024-01-03 17:41:09 +0300

    "use strict";  String Access multiline

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Katı Mod
+"use strict";
+
+// Katı mod izin vermiyor
+// deneme="Miuul Node js";
+// console.log(deneme);
+
 // Hello World
 console.log("Miuul Hello World!");
 
@@ -8,3 +15,35 @@ let number2=2;
 console.log(number2);
 console.log(number2);
 console.log(number2);
+
+////////////////////////////////number1
+// ES5
+// "use strict";
+// String Access
+// multiline
+// Date.now()
+
+
+//ES5(2009) 1-  "use strict";
+
+//ES5(2009) 2- String Access
+let data1="Miuul Node JS";
+console.log(data1.charAt(0));
+console.log(data1[0]); // String Access
+
+let multiLineString="Bu 1.satır\n"+
+"2.satır\n"+
+"3.satır\n"
+console.log(multiLineString);
+
+
+//ES5(2009) 3-  multiline escape character kullanmadım
+let multiLine=`Bu 1.satır
+2.satır
+3.satır
+`;
+console.log(multiLine);
+
+//ES5(2009) 4-  Date now
+let currentTimeMillis=Date.now();
+console.log(currentTimeMillis);
