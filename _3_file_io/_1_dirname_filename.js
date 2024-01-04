4bf4f30ec3a1b2175b8472eb8dc9ae247843fbe3
Hamit Mizrak <hamitmizrak@gmail.com>
2024-01-04 20:32:10 +0300

    __dirname, __filename

---
+++ _3_file_io/_1_dirname_filename.js
@@ -0,0 +1,17 @@
+// __filename 
+// __dirname 
+
+// Filename 
+console.log(__filename);
+const fileNameValue=__filename;
+console.log("File Value: "+fileNameValue);
+console.log("File Value: "+fileNameValue.substring(0,12));
+
+
+// Dirname
+console.log(__dirname);
+const dirNameValue=__dirname;
+console.log("Dir Value: "+dirNameValue);
+console.log("Dir Value: "+dirNameValue.substring(0,12));
+
+// nodemon _1_dirname_filename
